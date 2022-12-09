dbc67560259044646fe350417eaa00145c44b324
Tebogo Nomnqa <tebzax2@gmail.com>
2022-12-09 20:51:11 +0200

    feat: add Nextjs Head component in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hi World!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Index Page</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="index page description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hi World!</h1>
+      </main>
+    </>
+  );
 }
